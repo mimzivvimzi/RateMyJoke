8cdae313749ef07d2208f53ad76c84c9050c61d5
Michelle Lau <mdeniselau@gmail.com>
2020-07-05 15:18:49 +0900

    Initial Commit

---
+++ RateMyJoke/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RateMyJoke
+//
+//  Created by Michelle Lau on 2020/07/05.
+//  Copyright © 2020 Michelle Lau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
